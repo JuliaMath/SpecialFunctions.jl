49fc90354926c566d6e8c1e61006b58311788f7a
Alex Arslan <ararslan@comcast.net>
2016-06-02 15:42:58 -0700

    Explicitly use the functions from this module in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using SpecialFunctions
 using Base.Test
 
 # override Main
-for f in names(SpecialFunctions)
-    if isdefined(Main,f)
-        @eval global const $f = SpecialFunctions.$f
-    end
-end
+# for f in names(SpecialFunctions)
+#     if isdefined(Main,f)
+#         @eval global const $f = SpecialFunctions.$f
+#     end
+# end
 
 include("bessel.jl")
