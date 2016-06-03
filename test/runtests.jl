370368de37dd08ddff3984b9e2e344281af38240
Alex Arslan <ararslan@comcast.net>
2016-06-02 17:34:27 -0700

    Hopefully fix 0.5 builds by swapping out deprecated things (#8)
    * Swapped out deprecated features
    
    * Explicitly use the functions from this module in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,11 @@
-include("../src/SpecialFunctions.jl")
-import SpecialFunctions
+using SpecialFunctions
 using Base.Test
 
 # override Main
-for f in names(SpecialFunctions)
-    if isdefined(Main,f)
-        @eval global const $f = SpecialFunctions.$f
-    end
-end
+# for f in names(SpecialFunctions)
+#     if isdefined(Main,f)
+#         @eval global const $f = SpecialFunctions.$f
+#     end
+# end
 
 include("bessel.jl")
