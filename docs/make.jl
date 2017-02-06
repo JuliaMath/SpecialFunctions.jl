9e0358633b8d77371c36258dfa35598bc7c501cc
Alex Arslan <ararslan@comcast.net>
2017-02-06 15:50:04 -0800

    Adjustments, add Documenter

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using SpecialFunctions, Documenter
+
+makedocs(
+    modules = [SpecialFunctions],
+    clean = false,
+    format = :html,
+    sitename = "SpecialFunctions.jl",
+    authors = "Jeff Bezanson, Stefan Karpinski, Viral B. Shah, et al.",
+    pages = [
+        "Home" => "index.md",
+        "Functions" => "special.md",
+    ],
+)
+
+deploydocs(
+    repo = "https://github.com/JuliaMath/SpecialFunctions.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+)
--- src/SpecialFunctions.jl
+++ src/SpecialFunctions.jl
@@ -4,52 +4,56 @@ module SpecialFunctions
 
 # On older versions these are exported from Base
 # TODO: Uncomment me once these have been removed!
-# if VERSION >= v"0.6.0-dev.XXXX"
-#     const openspecfun = "libopenspecfun"
-#
-#     export
-#         airyai,
-#         airyaiprime,
-#         airybi,
-#         airybiprime,
-#         airyaix,
-#         airyaiprimex,
-#         airybix,
-#         airybiprimex,
-#         besselh,
-#         besselhx,
-#         besseli,
-#         besselix,
-#         besselj,
-#         besselj0,
-#         besselj1,
-#         besseljx,
-#         besselk,
-#         besselkx,
-#         bessely,
-#         bessely0,
-#         bessely1,
-#         besselyx,
-#         dawson,
-#         erf,
-#         erfc,
-#         erfcinv,
-#         erfcx,
-#         erfi,
-#         erfinv,
-#         eta,
-#         digamma,
-#         invdigamma,
-#         polygamma,
-#         trigamma,
-#         hankelh1,
-#         hankelh1x,
-#         hankelh2,
-#         hankelh2x,
-#         zeta
-# else
-#     const openspecfun = Base.Math.openspecfun
-# end
+#if VERSION >= v"0.6.0-dev.XXXX"
+#    export
+#        airyai,
+#        airyaiprime,
+#        airybi,
+#        airybiprime,
+#        airyaix,
+#        airyaiprimex,
+#        airybix,
+#        airybiprimex,
+#        besselh,
+#        besselhx,
+#        besseli,
+#        besselix,
+#        besselj,
+#        besselj0,
+#        besselj1,
+#        besseljx,
+#        besselk,
+#        besselkx,
+#        bessely,
+#        bessely0,
+#        bessely1,
+#        besselyx,
+#        dawson,
+#        erf,
+#        erfc,
+#        erfcinv,
+#        erfcx,
+#        erfi,
+#        erfinv,
+#        eta,
+#        digamma,
+#        invdigamma,
+#        polygamma,
+#        trigamma,
+#        hankelh1,
+#        hankelh1x,
+#        hankelh2,
+#        hankelh2x,
+#        zeta
+#end
+
+# On older versions libopenspecfun is included in Base
+# TODO: Uncomment me once Base no longer ships with it!
+#if VERSION >= v"0.6.0-dev.XXXX"
+#    const openspecfun = "libopenspecfun"
+#else
+    const openspecfun = Base.Math.openspecfun
+#end
 
 include("bessel.jl")
 include("erf.jl")
