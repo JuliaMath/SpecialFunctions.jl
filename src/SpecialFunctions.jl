0e21facef31768c2b83e366914bd487d15a862b3
Alex Arslan <ararslan@comcast.net>
2018-09-13 20:52:38 -0700

    Simplify logic now that we require 0.7

--- src/SpecialFunctions.jl
+++ src/SpecialFunctions.jl
@@ -1,75 +1,59 @@
-__precompile__()
 module SpecialFunctions
 
-using Compat
-
-if VERSION >= v"0.7.0-DEV.1760"
-    # Load openspecfun libraries from our deps.jl
-    const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+# Load openspecfun libraries from our deps.jl
+let depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
     if !isfile(depsjl_path)
-        error("SpecialFunctions not installed properly, run Pkg.build(\"SpecialFunctions\"), restart Julia and try again")
+        error("SpecialFunctions is not installed properly, run `Pkg.build(\"SpecialFunctions\")`," *
+              "restart Julia and try again")
     end
     include(depsjl_path)
-else
-    using Base.Math: openspecfun
 end
 
-if isdefined(Base, :airyai) && VERSION < v"0.7.0-DEV.986" #22763
-    import Base: airyai, airyaix, airyaiprime, airyaiprimex,
-                 airybi, airybix, airybiprime, airybiprimex,
-                 besselh, besselhx, besseli, besselix, besselj, besselj0, besselj1,
-                 besseljx, besselk, besselkx, bessely, bessely0, bessely1, besselyx,
-                 hankelh1, hankelh1x, hankelh2, hankelh2x,
-                 dawson, erf, erfc, erfcinv, erfcx, erfi, erfinv,
-                 eta, digamma, invdigamma, polygamma, trigamma, zeta,
-                 # deprecated
-                 airy, airyx, airyprime
-else
-    export
-        airyai,
-        airyaiprime,
-        airybi,
-        airybiprime,
-        airyaix,
-        airyaiprimex,
-        airybix,
-        airybiprimex,
-        besselh,
-        besselhx,
-        besseli,
-        besselix,
-        besselj,
-        besselj0,
-        besselj1,
-        besseljx,
-        besselk,
-        besselkx,
-        bessely,
-        bessely0,
-        bessely1,
-        besselyx,
-        dawson,
-        erf,
-        erfc,
-        erfcinv,
-        erfcx,
-        erfi,
-        erfinv,
-        eta,
-        digamma,
-        invdigamma,
-        polygamma,
-        trigamma,
-        hankelh1,
-        hankelh1x,
-        hankelh2,
-        hankelh2x,
-        zeta
-end
+__init__() = check_deps()
 
-export sinint,
-       cosint,
-       lbinomial
+export
+    airyai,
+    airyaiprime,
+    airybi,
+    airybiprime,
+    airyaix,
+    airyaiprimex,
+    airybix,
+    airybiprimex,
+    besselh,
+    besselhx,
+    besseli,
+    besselix,
+    besselj,
+    besselj0,
+    besselj1,
+    besseljx,
+    besselk,
+    besselkx,
+    bessely,
+    bessely0,
+    bessely1,
+    besselyx,
+    dawson,
+    erf,
+    erfc,
+    erfcinv,
+    erfcx,
+    erfi,
+    erfinv,
+    eta,
+    digamma,
+    invdigamma,
+    polygamma,
+    trigamma,
+    hankelh1,
+    hankelh1x,
+    hankelh2,
+    hankelh2x,
+    zeta,
+    sinint,
+    cosint,
+    lbinomial
 
 include("bessel.jl")
 include("erf.jl")
@@ -77,17 +61,15 @@ include("sincosint.jl")
 include("gamma.jl")
 include("deprecated.jl")
 
-@static if isdefined(Base, :missing)
-    for f in (:digamma, :erf, :erfc, :erfcinv, :erfcx, :erfi, :erfinv,
-              :eta, :gamma, :invdigamma, :lfactorial, :lgamma, :trigamma)
-        @eval $(f)(::Missing) = missing
-    end
-    for f in (:beta, :lbeta)
-        @eval $(f)(::Number, ::Missing) = missing
-        @eval $(f)(::Missing, ::Number) = missing
-        @eval $(f)(::Missing, ::Missing) = missing
-    end
-    polygamma(m::Integer, x::Missing) = missing
+for f in (:digamma, :erf, :erfc, :erfcinv, :erfcx, :erfi, :erfinv,
+          :eta, :gamma, :invdigamma, :lfactorial, :lgamma, :trigamma)
+    @eval $(f)(::Missing) = missing
 end
+for f in (:beta, :lbeta)
+    @eval $(f)(::Number, ::Missing) = missing
+    @eval $(f)(::Missing, ::Number) = missing
+    @eval $(f)(::Missing, ::Missing) = missing
+end
+polygamma(m::Integer, x::Missing) = missing
 
 end # module
