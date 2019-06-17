8d3b530f1af67c9d541c264f0bdaf0eabf2958c0
Paul Luckner <paul.luckner@rwth-aachen.de>
2019-06-17 21:43:39 +0200

    updated deprecated makedocs call: format; clean build dir; aligned code

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,16 @@
 using SpecialFunctions, Documenter
 
 makedocs(
-    modules = [SpecialFunctions],
-    clean = false,
-    format = :html,
-    sitename = "SpecialFunctions.jl",
-    authors = "Jeff Bezanson, Stefan Karpinski, Viral B. Shah, et al.",
-    pages = [
-        "Home" => "index.md",
+    modules     = [SpecialFunctions],
+    sitename    = "SpecialFunctions.jl",
+    authors     = "Jeff Bezanson, Stefan Karpinski, Viral B. Shah, et al.",
+    pages       = [
+        "Home"      => "index.md",
         "Functions" => "special.md",
     ],
 )
 
 deploydocs(
-    repo = "github.com/JuliaMath/SpecialFunctions.jl.git",
-    target = "build",
+    repo    = "github.com/JuliaMath/SpecialFunctions.jl.git",
+    target  = "build",
 )
