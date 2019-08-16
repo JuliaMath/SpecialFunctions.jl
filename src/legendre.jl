d3a5e4acf5104d2a9b905c6362be3ddcb4bfcfa6
PaulXiCao <25491255+PaulXiCao@users.noreply.github.com>
2019-08-16 08:54:19 +0200

    using rationals in Bonnet recursion

--- src/legendre.jl
+++ src/legendre.jl
@@ -40,7 +40,7 @@ function legendreP_bonnet(n::Integer, x::Real)
     elseif n == 1
         return x
     else
-        return  ((2*n-1)/n) * x * legendreP_bonnet(n-1, x) -
-                ((n-1)/n)       * legendreP_bonnet(n-2, x)
+        return  ((2*n-1)//n) * x * legendreP_bonnet(n-1, x) -
+                ((n-1)//n)       * legendreP_bonnet(n-2, x)
     end
 end
