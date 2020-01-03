230180438d6479391c5ddbb3e831b2076cdfe45d
Ashton Bradley <ashton.bradley@gmail.com>
2020-01-04 08:16:07 +1300

    Update src/bessel.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- src/bessel.jl
+++ src/bessel.jl
@@ -693,7 +693,7 @@ hankelh1(nu, z) = besselh(nu, 1, z)
 
 Bessel function of the third kind of order `nu`, ``H^{(2)}_\\nu(x)``.
 
-External links: [DLMF](https://dlmf.nist.gov/10.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
+External links: [DLMF](https://dlmf.nist.gov/10.2.6), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
 
 See also: [`hankelh2x(nu,x)`](@ref SpecialFunctions.hankelh2x)
 """
