ada68996acc3d3060e0e96fe593b87d08834e025
Ashton Bradley <ashton.bradley@gmail.com>
2020-01-04 08:19:12 +1300

    Update src/bessel.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- src/bessel.jl
+++ src/bessel.jl
@@ -463,7 +463,7 @@ end
 
 Bessel function of the first kind of order `nu`, ``J_\\nu(x)``.
 
-External links: [DLMF](https://dlmf.nist.gov/10.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Bessel_functions_of_the_first_kind:_J%CE%B1)
+External links: [DLMF](https://dlmf.nist.gov/10.2.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Bessel_functions_of_the_first_kind:_J%CE%B1)
 
 See also: [`besseljx(nu,x)`](@ref SpecialFunctions.besseljx), [`besseli(nu,x)`](@ref SpecialFunctions.besseli), [`besselk(nu,x)`](@ref SpecialFunctions.besselk)
 """
