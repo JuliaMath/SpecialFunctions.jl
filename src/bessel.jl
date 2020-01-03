26cedd7616a554d5dd4518df2a174773bf313027
Ashton Bradley <ashton.bradley@gmail.com>
2020-01-04 08:22:56 +1300

    Update src/bessel.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- src/bessel.jl
+++ src/bessel.jl
@@ -300,7 +300,7 @@ Bessel function of the third kind of order `nu` (the Hankel function). `k` is ei
 selecting [`hankelh1`](@ref) or [`hankelh2`](@ref), respectively.
 `k` defaults to 1 if it is omitted.
 
-External links: [DLMF](https://dlmf.nist.gov/10.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
+External links: [DLMF](https://dlmf.nist.gov/10.2.5) and [DLMF](https://dlmf.nist.gov/10.2.6), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
 
 See also: [`besselhx`](@ref) for an exponentially scaled variant.
 """
