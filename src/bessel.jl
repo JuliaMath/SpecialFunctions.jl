df12e5a9a6c31adb043b17273770d6e2311a0ce2
Ashton Bradley <ashton.bradley@gmail.com>
2020-01-04 08:15:33 +1300

    Update src/bessel.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- src/bessel.jl
+++ src/bessel.jl
@@ -682,7 +682,7 @@ end
 
 Bessel function of the third kind of order `nu`, ``H^{(1)}_\\nu(x)``.
 
-External links: [DLMF](https://dlmf.nist.gov/10.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
+External links: [DLMF](https://dlmf.nist.gov/10.2.5), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Hankel_functions:_H(1)%CE%B1,_H(2)%CE%B1)
 
 See also: [`hankelh1x`](@ref SpecialFunctions.hankelh1x)
 """
