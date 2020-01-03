f84bc6ddeabebf1651800e98b00c20e649c2b05e
Ashton Bradley <ashton.bradley@gmail.com>
2020-01-04 08:21:02 +1300

    Update src/bessel.jl
    Co-Authored-By: Simon Byrne <simonbyrne@gmail.com>

--- src/bessel.jl
+++ src/bessel.jl
@@ -616,7 +616,7 @@ end
 
 Bessel function of the first kind of order 1, ``J_1(x)``.
 
-External links: [DLMF](https://dlmf.nist.gov/10.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Bessel_functions_of_the_first_kind:_J%CE%B1)
+External links: [DLMF](https://dlmf.nist.gov/10.2.2), [Wikipedia](https://en.wikipedia.org/wiki/Bessel_function#Bessel_functions_of_the_first_kind:_J%CE%B1)
 
 See also: [`besselj(nu,x)`](@ref SpecialFunctions.besselj)
 """
