1ad1cfa30cfce0acab717ba6ca7cb4db8af3309c
Bryce Wiedenbeck <brwiedenbeck@davidson.edu>
2020-05-29 22:57:26 -0400

    added logmultinomial function (compare to Combinatorics.multinomial)

--- src/gamma.jl
+++ src/gamma.jl
@@ -566,7 +566,7 @@ function polygamma(m::Integer, z::Number)
     polygamma(m, x)
 end
 
-export gamma, loggamma, logabsgamma, beta, logbeta, logabsbeta, logfactorial, logabsbinomial
+export gamma, loggamma, logabsgamma, beta, logbeta, logabsbeta, logfactorial, logabsbinomial, logmultinomial
 
 ## from base/special/gamma.jl
 
@@ -922,3 +922,25 @@ function logabsbinomial(n::T, k::T) where {T<:Integer}
     end
 end
 logabsbinomial(n::Integer, k::Integer) = logabsbinomial(promote(n, k)...)
+
+#=
+logmultinomial computes the log of the multinomial coefficient.
+From Wolfram Mathworld (https://mathworld.wolfram.com/MultinomialCoefficient.html):
+
+The multinomial coefficients
+
+$$(n_1, n_2, \ldots, n_k)! = \frac{(n_1+n_2+...+n_k)!}{n_1! n_2! \ldots n_k!}$$
+
+are the terms in the multinomial series expansion. In other words, the number
+of distinct permutations in a multiset of $k$ distinct elements of multiplicity
+$n_i$ $(1 \le i \le k)$ is $(n_1, \ldots, n_k)!$.
+=#
+function logmultinomial(multiset...)
+    numerator = 0
+    denominator = 0.0
+    @inbounds for multiplicity in multiset
+        numerator += multiplicity
+        denominator += loggamma(multiplicity + 1)
+    end
+    return loggamma(numerator + 1) - denominator
+end
