86e7b03088b7592817408f654f60274f6640c899
Steven G. Johnson <stevenj@mit.edu>
2020-09-18 14:32:53 -0400

    Update beta_inc.jl

--- src/beta_inc.jl
+++ src/beta_inc.jl
@@ -9,7 +9,10 @@ const exparg_p =  log(prevfloat(floatmax(Float64)))
 
 Computes ``log(\\Gamma(b)/\\Gamma(a+b))`` when b >= 8
 """
-function loggammadiv(a::Float64, b::Float64)
+loggammadiv(a::Number, b::Number) = _loggammadiv(float(a), float(b))
+
+_loggammadiv(a::T, b::T) where {T<:Base.IEEEFloat} = T(_loggammadiv(Float64(a), Float64(b)) # handle Float16, Float32
+function _loggammadiv(a::Float64, b::Float64)
     if a > b
         h = b/a
         c = 1.0/(1.0 + h)
