733cc98d0d89eba431cb954fad70749eb4370602
Steven G. Johnson <stevenj@alum.mit.edu>
2020-11-05 15:19:49 -0500

    more expint fixes

--- src/expint.jl
+++ src/expint.jl
@@ -187,7 +187,7 @@ function En_cf_nogamma(ν::Number, z::Number, n::Int=1000)
     end
 
     exppart = exp(-z)
-    if abs(real(exppart)) == Inf && abs(imag(exppart)) == Inf
+    if isinf(exppart)
         return exp(-z + log(A) - log(B)), iters
     else
         cfpart = A/B
@@ -235,7 +235,7 @@ function En_cf_gamma(ν::Number, z::Number, n::Int=1000)
 
     gammapart = En_safe_gamma_term(ν, z)
     cfpart = exp(-z)
-    if abs(real(cfpart)) == Inf || abs(imag(cfpart)) == Inf
+    if isinf(cfpart)
         factor = sign(real(cfpart)) + sign(imag(cfpart))*im
         cfpart = exp(-z + log(B) - log(A))
     else
@@ -351,7 +351,6 @@ function En_imagbranchcut(ν::Number, z::Number)
     return imag(impart) * im # get rid of any real error
 end
 
-const ORIGIN_EXPAND_THRESH = 3
 """
     expint(z)
     expint(ν, z)
@@ -384,13 +383,13 @@ function expint(ν::Number, z::Number, niter::Int=1000)
     elseif ν == 1 && real(z) > 0 && z isa Union{Float64, Complex{Float64}}
         return expint_opt(z)
     end
-    # asymptotic test for |z| → ∞
+    # asymptotic test for underflow when Re z → ∞
     # https://functions.wolfram.com/GammaBetaErf/ExpIntegralE/06/02/0003/
-    if exp(-z) / z == 0
+    if real(z) > -log(nextfloat(zero(real(z))))+1 # exp(-z) is zero
         return zero(z)
     end
 
-    if abs(z) < ORIGIN_EXPAND_THRESH
+    if abs2(z) < 9
         # use Taylor series about the origin for small z
         return En_expand_origin(ν, z)
     end
@@ -413,6 +412,7 @@ function expint(ν::Number, z::Number, niter::Int=1000)
         imstart = (imz == 0) ? abs(z)*sqrt(eps(typeof(real(z)))) : imz
         z₀ = rez + imstart*im
         E_start, i, _ = En_cf(ν, z₀, quick_niter)
+        isnan(E_start) && return E_start
         if imz > 0 && i < nmax
             # didn't need to take any steps
             return doconj ? conj(E_start) : E_start
