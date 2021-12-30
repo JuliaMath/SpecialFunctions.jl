8bc181496d2a9362d129e838c799579591db8101
Alexey Stukalov <astukalov@gmail.com>
2021-12-31 00:20:34 +0100

    sync with latest LambertW.jl master

--- src/lambertw.jl
+++ src/lambertw.jl
@@ -1,14 +1,130 @@
-import Base: convert
-
-using Compat
-import Compat.MathConstants  # For clarity, we use MathConstants.e for Euler's number
-
 #### Lambert W function ####
 
+"""
+    lambertw(z::Complex{T}, k::V=0, maxits=1000) where {T<:Real, V<:Integer}
+    lambertw(z::T, k::V=0, maxits=1000) where {T<:Real, V<:Integer}
+
+Compute the `k`th branch of the Lambert W function of `z`. If `z` is real, `k` must be
+either `0` or `-1`. For `Real` `z`, the domain of the branch `k = -1` is `[-1/e, 0]` and the
+domain of the branch `k = 0` is `[-1/e, Inf]`. For `Complex` `z`, and all `k`, the domain is
+the complex plane.
+
+```jldoctest
+julia> lambertw(-1/e, -1)
+-1.0
+
+julia> lambertw(-1/e, 0)
+-1.0
+
+julia> lambertw(0, 0)
+0.0
+
+julia> lambertw(0, -1)
+-Inf
+
+julia> lambertw(Complex(-10.0, 3.0), 4)
+-0.9274337508660128 + 26.37693445371142im
+```
+"""
+lambertw(z, k::Integer=0, maxits::Integer=1000) = _lambertw(float(z), k, maxits)
+
+# lambertw(e + 0im, k) is ok for all k
+# Maybe this should return a float. But, this should cause no type instability in any case
+function _lambertw(::typeof(MathConstants.e), k, maxits)
+    k == 0 && return 1
+    throw(DomainError(k))
+end
+_lambertw(x::Irrational, k, maxits) = _lambertw(float(x), k, maxits)
+function _lambertw(x::Union{Integer, Rational}, k, maxits)
+    if k == 0
+        x == 0 && return float(zero(x))
+        x == 1 && return convert(typeof(float(x)), omega) # must be a more efficient way
+    end
+    return _lambertw(float(x), k, maxits)
+end
+
+### Real x
+
+function _lambertw(x::Real, k, maxits)
+    k == 0 && return lambertw_branch_zero(x, maxits)
+    k == -1 && return lambertw_branch_one(x, maxits)
+    throw(DomainError(k, "lambertw: real x must have branch k == 0 or k == -1"))
+end
+
+# Real x, k = 0
+# This appears to be inferrable with T=Float64 and T=BigFloat, including if x=Inf.
+# There is a magic number here. It could be noted, or possibly removed.
+# In particular, the fancy initial condition selection does not seem to help speed.
+function lambertw_branch_zero(x::T, maxits)::T where T<:Real
+    isnan(x) && return(NaN)
+    x == Inf && return Inf # appears to return convert(BigFloat, Inf) for x == BigFloat(Inf)
+    one_t = one(T)
+    oneoe = -one_t / convert(T, MathConstants.e)  # The branch point
+    x == oneoe && return -one_t
+    oneoe <= x || throw(DomainError(x))
+    itwo_t = 1 / convert(T, 2)
+    if x > one_t
+        lx = log(x)
+        llx = log(lx)
+        x0 = lx - llx - log(one_t - llx / lx) * itwo_t
+    else
+        x0 = (567//1000) * x
+    end
+    return lambertw_root_finding(x, x0, maxits)
+end
+
+# Real x, k = -1
+function lambertw_branch_one(x::T, maxits) where T<:Real
+    oneoe = -one(T) / convert(T, MathConstants.e)
+    x == oneoe && return -one(T) # W approaches -1 as x -> -1/e from above
+    oneoe <= x || throw(DomainError(x))  # branch domain exludes x < -1/e
+    x == zero(T) && return -convert(T, Inf) # W decreases w/o bound as x -> 0 from below
+    x < zero(T) || throw(DomainError(x))
+    return lambertw_root_finding(x, log(-x), maxits)
+end
+
+### Complex z
+
+_lambertw(z::Complex{<:Integer}, k, maxits) = _lambertw(float(z), k, maxits)
+# choose initial value inside correct branch for root finding
+function _lambertw(z::Complex{T}, k, maxits) where T<:Real
+    one_t = one(T)
+    local w::Complex{T}
+    pointseven = 7//10
+    if abs(z) <= one_t/convert(T, MathConstants.e)
+        if z == 0
+            k == 0 && return z
+            return complex(-convert(T, Inf), zero(T))
+        end
+        if k == 0
+            w = z
+        elseif k == -1 && imag(z) == 0 && real(z) < 0
+            w = complex(log(-real(z)), 1//10^7) # need offset for z ≈ -1/e.
+        else
+            w = log(z)
+            k != 0 ? w += complex(0, k * 2 * pi) : nothing
+        end
+    elseif k == 0 && imag(z) <= pointseven && abs(z) <= pointseven
+        w = abs(z+ 1//2) < 1//10 ? imag(z) > 0 ? complex(pointseven, pointseven) : complex(pointseven, -pointseven) : z
+    else
+        if real(z) == convert(T, Inf)
+            k == 0 && return z
+            return z + complex(0, 2*k*pi)
+        end
+        real(z) == -convert(T, Inf) && return -z + complex(0, (2*k+1)*pi)
+        w = log(z)
+        k != 0 ? w += complex(0, 2*k*pi) : nothing
+    end
+    return lambertw_root_finding(z, w, maxits)
+end
+
+### root finding, iterative solution
+
 # Use Halley's root-finding method to find
-# x = lambertw(z) with initial point x.
-function _lambertw(z::T, x::T, maxits) where T <: Number
-    two_t = convert(T,2)
+# x = lambertw(z) with initial point x0.
+function lambertw_root_finding(z::T, x0::T, maxits) where T <: Number
+    two_t = convert(T, 2)
+    x = x0
     lastx = x
     lastdiff = zero(T)
     converged::Bool = false
@@ -16,146 +132,27 @@ function _lambertw(z::T, x::T, maxits) where T <: Number
         ex = exp(x)
         xexz = x * ex - z
         x1 = x + 1
-        x -= xexz / (ex * x1 - (x + two_t) * xexz / (two_t * x1 ) )
+        x -= xexz / (ex * x1 - (x + two_t) * xexz / (two_t * x1 ))
         xdiff = abs(lastx - x)
-        if xdiff <= 3*eps(abs(lastx)) || lastdiff == xdiff  # second condition catches two-value cycle
+        if xdiff <= 3 * eps(abs(lastx)) || lastdiff == xdiff  # second condition catches two-value cycle
             converged = true
             break
         end
         lastx = x
         lastdiff = xdiff
     end
-    converged || warn("lambertw with z=", z, " did not converge in ", maxits, " iterations.")
+    converged || @warn("lambertw with z=", z, " did not converge in ", maxits, " iterations.")
     return x
 end
 
-### Real z ###
+### omega constant
 
-# Real x, k = 0
-# This appears to be inferrable with T=Float64 and T=BigFloat, including if x=Inf.
-# The fancy initial condition selection does not seem to help speed, but we leave it for now.
-function lambertwk0(x::T, maxits)::T where T<:AbstractFloat
-    isnan(x) && return(NaN)
-    x == Inf && return Inf # appears to return convert(BigFloat,Inf) for x == BigFloat(Inf)
-    one_t = one(T)
-    oneoe = -one_t/convert(T,MathConstants.e)  # The branch point
-    x == oneoe && return -one_t
-    oneoe <= x || throw(DomainError(x))
-    itwo_t = 1/convert(T,2)
-    if x > one_t
-        lx = log(x)
-        llx = log(lx)
-        x1 = lx - llx - log(one_t - llx/lx) * itwo_t
-    else
-        x1 = (567//1000) * x
-    end
-    return _lambertw(x, x1, maxits)
-end
+const _omega_const = 0.567143290409783872999968662210355
 
-# Real x, k = -1
-function lambertwkm1(x::T, maxits) where T<:Real
-    oneoe = -one(T)/convert(T,MathConstants.e)
-    x == oneoe && return -one(T) # W approaches -1 as x -> -1/e from above
-    oneoe <= x || throw(DomainError(x))  # branch domain exludes x < -1/e
-    x == zero(T) && return -convert(T,Inf) # W decreases w/o bound as x -> 0 from below
-    x < zero(T) || throw(DomainError(x))
-    return _lambertw(x, log(-x), maxits)
-end
-
-"""
-    lambertw(z::Complex{T}, k::V=0, maxits=1000) where {T<:Real, V<:Integer}
-    lambertw(z::T, k::V=0, maxits=1000) where {T<:Real, V<:Integer}
-
-Compute the `k`th branch of the Lambert W function of `z`. If `z` is real, `k` must be
-either `0` or `-1`. For `Real` `z`, the domain of the branch `k = -1` is `[-1/e,0]` and the
-domain of the branch `k = 0` is `[-1/e,Inf]`. For `Complex` `z`, and all `k`, the domain is
-the complex plane. When using root finding to compute `W`, a value for `W` is returned
-with a warning if it has not converged after `maxits` iterations.
-
-```jldoctest
-julia> lambertw(-1/e,-1)
--1.0
-
-julia> lambertw(-1/e,0)
--1.0
-
-julia> lambertw(0,0)
-0.0
-
-julia> lambertw(0,-1)
--Inf
-
-julia> lambertw(Complex(-10.0,3.0), 4)
--0.9274337508660128 + 26.37693445371142im
-```
-
-"""
-lambertw(z, k::Integer=0, maxits::Integer=1000) = lambertw_(z, k, maxits)
-
-function lambertw_(x::Real, k, maxits)
-    k == 0 && return lambertwk0(x, maxits)
-    k == -1 && return lambertwkm1(x, maxits)
-    throw(DomainError(k, "lambertw: real x must have branch k == 0 or k == -1"))
-end
-
-function lambertw_(x::Union{Integer,Rational}, k, maxits)
-    if k == 0
-        x == 0 && return float(zero(x))
-        x == 1 && return convert(typeof(float(x)), omega) # must be a more efficient way
-    end
-    return lambertw_(float(x), k, maxits)
-end
-
-### Complex z ###
-
-# choose initial value inside correct branch for root finding
-function lambertw_(z::Complex{T}, k, maxits) where T<:Real
-    one_t = one(T)
-    local w::Complex{T}
-    pointseven = 7//10
-    if abs(z) <= one_t/convert(T,MathConstants.e)
-        if z == 0
-            k == 0 && return z
-            return complex(-convert(T,Inf),zero(T))
-        end
-        if k == 0
-            w = z
-        elseif k == -1 && imag(z) == 0 && real(z) < 0
-            w = complex(log(-real(z)),1//10^7) # need offset for z ≈ -1/e.
-        else
-            w = log(z)
-            k != 0 ? w += complex(0,k * 2 * pi) : nothing
-        end
-    elseif k == 0 && imag(z) <= pointseven && abs(z) <= pointseven
-        w = abs(z+ 1//2) < 1//10 ? imag(z) > 0 ? complex(pointseven,pointseven) : complex(pointseven,-pointseven) : z
-    else
-        if real(z) == convert(T,Inf)
-            k == 0 && return z
-            return z + complex(0,2*k*pi)
-        end
-        real(z) == -convert(T,Inf) && return -z + complex(0,(2*k+1)*pi)
-        w = log(z)
-        k != 0 ? w += complex(0, 2*k*pi) : nothing
-    end
-    return _lambertw(z, w, maxits)
-end
-
-lambertw_(z::Complex{T}, k, maxits) where T<:Integer = lambertw_(float(z), k, maxits)
-lambertw_(n::Irrational, k, maxits) = lambertw_(float(n), k, maxits)
-
-# lambertw(e + 0im,k) is ok for all k
-# Maybe this should return a float. But, this should cause no type instability in any case
-function lambertw_(::typeof(MathConstants.e), k, maxits)
-    k == 0 && return 1
-    throw(DomainError(k))
-end
-
-### omega constant ###
-
-const omega_const_ = 0.567143290409783872999968662210355
 # The BigFloat `omega_const_bf_` is set via a literal in the function __init__ to prevent a segfault
 
-# maybe compute higher precision. converges very quickly
+# compute omega constant via root finding
+# We could compute higher precision. This converges very quickly.
 function omega_const(::Type{BigFloat})
     precision(BigFloat) <= 256 && return omega_const_bf_[]
     myeps = eps(BigFloat)
@@ -174,6 +171,7 @@ end
 
 The constant defined by `ω exp(ω) = 1`.
 
+# Example
 ```jldoctest
 julia> ω
 ω = 0.5671432904097...
@@ -191,17 +189,12 @@ julia> big(omega)
 const ω = Irrational{:ω}()
 @doc (@doc ω) omega = ω
 
-# The following two lines may be removed when support for v0.6 is dropped
-Base.convert(::Type{AbstractFloat}, o::Irrational{:ω}) = Float64(o)
-Base.convert(::Type{Float16}, o::Irrational{:ω}) = Float16(o)
-Base.convert(::Type{T}, o::Irrational{:ω}) where T <:Number = T(o)
+Base.Float64(::Irrational{:ω}) = _omega_const
+Base.Float32(::Irrational{:ω}) = Float32(_omega_const)
+Base.Float16(::Irrational{:ω}) = Float16(_omega_const)
+Base.BigFloat(::Irrational{:ω}) = omega_const(BigFloat)
 
-Base.Float64(::Irrational{:ω}) = omega_const_  # FIXME: This is very slow. Why ?
-Base.Float32(::Irrational{:ω}) = Float32(omega_const_)
-Base.Float16(::Irrational{:ω}) = Float16(omega_const_)
-Base.BigFloat(o::Irrational{:ω}) = omega_const(BigFloat)
-
-### Expansion about branch point x = -1/e  ###
+### Expansion about branch point x = -1/e
 
 # Refer to the paper "On the Lambert W function". In (4.22)
 # coefficients μ₀ through μ₃ are given explicitly. Recursion relations
@@ -209,83 +202,78 @@ Base.BigFloat(o::Irrational{:ω}) = omega_const(BigFloat)
 # recursion relations.
 
 # (4.23) and (4.24) give zero based coefficients.
-cset(a,i,v) = a[i+1] = v
-cget(a,i) = a[i+1]
+cset(a, i, v) = a[i+1] = v
+cget(a, i) = a[i+1]
 
 # (4.24)
-function compa(k,m,a)
+function compute_a_coeffs(k, m, a)
     sum0 = zero(eltype(m))
-    for j in 2:k-1
-        sum0 += cget(m,j) * cget(m,k+1-j)
+    for j in 2:(k - 1)
+        sum0 += cget(m, j) * cget(m, k + 1 - j)
     end
-    cset(a,k,sum0)
+    cset(a, k, sum0)
     return sum0
 end
 
 # (4.23)
-function compm(k,m,a)
-    kt = convert(eltype(m),k)
-    mk = (kt-1)/(kt+1) *(cget(m,k-2)/2 + cget(a,k-2)/4) -
-        cget(a,k)/2 - cget(m,k-1)/(kt+1)
-    cset(m,k,mk)
+function compute_m_coefficients(k, m, a)
+    kt = convert(eltype(m), k)
+    mk = (kt - 1) / (kt + 1) *(cget(m, k - 2) / 2 + cget(a, k - 2) / 4) -
+        cget(a, k) / 2 - cget(m, k - 1) / (kt + 1)
+    cset(m, k, mk)
     return mk
 end
 
 # We plug the known value μ₂ == -1//3 for (4.22) into (4.23) and
 # solve for α₂. We get α₂ = 0.
-# compute array of coefficients μ in (4.22).
+# Compute array of coefficients μ in (4.22).
 # m[1] is μ₀
-function lamwcoeff(T::DataType, n::Int)
-    # a = @compat Array{T}(undef,n)
-    # m = @compat Array{T}(undef,n)
-    a = zeros(T,n) # We don't need initialization, but Compat is a huge PITA.
-    m = zeros(T,n)
-    cset(a,0,2)  # α₀ literal in paper
-    cset(a,1,-1) # α₁ literal in paper
-    cset(a,2,0)  # α₂ get this by solving (4.23) for alpha_2 with values printed in paper
-    cset(m,0,-1) # μ₀ literal in paper
-    cset(m,1,1)  # μ₁ literal in paper
-    cset(m,2,-1//3) # μ₂ literal in paper, but only in (4.22)
-    for i in 3:n-1  # coeffs are zero indexed
-        compa(i,m,a)
-        compm(i,m,a)
+function compute_branch_point_coeffs(T::DataType, n::Int)
+    a =  Array{T}(undef, n)
+    m =  Array{T}(undef, n)
+    cset(a, 0, 2)  # α₀ literal in paper
+    cset(a, 1, -1) # α₁ literal in paper
+    cset(a, 2, 0)  # α₂ get this by solving (4.23) for alpha_2 with values printed in paper
+    cset(m, 0, -1) # μ₀ literal in paper
+    cset(m, 1, 1)  # μ₁ literal in paper
+    cset(m, 2, -1//3) # μ₂ literal in paper, but only in (4.22)
+    for i in 3:(n - 1)  # coeffs are zero indexed
+        compute_a_coeffs(i, m, a)
+        compute_m_coefficients(i, m, a)
     end
     return m
 end
 
-const LAMWMU_FLOAT64 = lamwcoeff(Float64,500)
+const BRANCH_POINT_COEFFS_FLOAT64 = compute_branch_point_coeffs(Float64, 500)
 
 # Base.Math.@horner requires literal coefficients
-# But, we have an array `p` of computed coefficients
-function horner(x, p::AbstractArray, n)
+# It cannot be used here because we have an array of computed coefficients
+function horner(x, coeffs::AbstractArray, n)
     n += 1
-    ex = p[n]
-    for i = n-1:-1:2
-        ex = :(muladd(t, $ex, $(p[i])))
+    ex = coeffs[n]
+    for i = (n - 1):-1:2
+        ex = :(muladd(t, $ex, $(coeffs[i])))
     end
     ex = :( t * $ex)
     return Expr(:block, :(t = $x), ex)
 end
 
-function mkwser(name, n)
-    iex = horner(:x,LAMWMU_FLOAT64,n)
-    return :(function ($name)(x) $iex  end)
+# write functions that evaluate the branch point series
+# with `num_terms` number of terms.
+for (func_name, num_terms) in (
+    (:wser3, 3), (:wser5, 5), (:wser7, 7), (:wser12, 12),
+    (:wser19, 19), (:wser26, 26), (:wser32, 32),
+    (:wser50, 50), (:wser100, 100), (:wser290, 290))
+    iex = horner(:x, BRANCH_POINT_COEFFS_FLOAT64, num_terms)
+    @eval function ($func_name)(x) $iex end
 end
 
-eval(mkwser(:wser3, 3))
-eval(mkwser(:wser5, 5))
-eval(mkwser(:wser7, 7))
-eval(mkwser(:wser12, 12))
-eval(mkwser(:wser19, 19))
-eval(mkwser(:wser26, 26))
-eval(mkwser(:wser32, 32))
-eval(mkwser(:wser50, 50))
-eval(mkwser(:wser100, 100))
-eval(mkwser(:wser290, 290))
-
 # Converges to Float64 precision
-# We could get finer tuning by separating k=0,-1 branches.
-function wser(p,x)
+# We could get finer tuning by separating k=0, -1 branches.
+# Why is wser5 omitted ?
+# p is the argument to the series which is computed
+# from x before calling `branch_point_series`.
+function branch_point_series(p, x)
     x < 4e-11 && return wser3(p)
     x < 1e-5 && return wser7(p)
     x < 1e-3 && return wser12(p)
@@ -294,12 +282,12 @@ function wser(p,x)
     x < 5e-2 && return wser32(p)
     x < 1e-1 && return wser50(p)
     x < 1.9e-1 && return wser100(p)
-    x > 1/MathConstants.e && throw(DomainError(x))  # radius of convergence
+    x > 1 / MathConstants.e && throw(DomainError(x))  # radius of convergence
     return wser290(p)  # good for x approx .32
 end
 
 # These may need tuning.
-function wser(p::Complex{T},z) where T<:Real
+function branch_point_series(p::Complex{T}, z) where T<:Real
     x = abs(z)
     x < 4e-11 && return wser3(p)
     x < 1e-5 && return wser7(p)
@@ -309,29 +297,31 @@ function wser(p::Complex{T},z) where T<:Real
     x < 5e-2 && return wser32(p)
     x < 1e-1 && return wser50(p)
     x < 1.9e-1 && return wser100(p)
-    x > 1/MathConstants.e && throw(DomainError(x))  # radius of convergence
+    x > 1 / MathConstants.e && throw(DomainError(x))  # radius of convergence
     return wser290(p)
 end
 
-@inline function _lambertw0(x) # 1 + W(-1/e + x)  , k = 0
-    ps = 2*MathConstants.e*x;
-    p = sqrt(ps)
-    return  wser(p,x)
+function _lambertw0(x) # 1 + W(-1/e + x)  , k = 0
+    ps = 2 * MathConstants.e * x
+    series_arg = sqrt(ps)
+    branch_point_series(series_arg, x)
 end
 
-@inline function _lambertwm1(x) # 1 + W(-1/e + x)  , k = -1
-    ps = 2*MathConstants.e*x;
-    p = -sqrt(ps)
-    return wser(p,x)
+function _lambertwm1(x) # 1 + W(-1/e + x)  , k = -1
+    ps = 2 * MathConstants.e * x
+    series_arg = -sqrt(ps)
+    branch_point_series(series_arg, x)
 end
 
 """
-    lambertwbp(z,k=0)
+    lambertwbp(z, k=0)
 
-Compute accurate value of `1 + W(-1/e + z)`, for `abs(z)` in `[0,1/e]` for `k` either `0` or `-1`.
+Compute accurate value of `1 + W(-1/e + z)`, for `abs(z)` in `[0, 1/e]` for `k` either `0` or `-1`.
+This function is faster and more accurate near the branch point `-1/e` between `k=0` and `k=1`.
 The result is accurate to Float64 precision for abs(z) < 0.32.
 If `k=-1` and `imag(z) < 0`, the value on the branch `k=1` is returned.
 
+# Example
 ```jldoctest
 julia> lambertw(-1/e + 1e-18, -1)
 -1.0
@@ -340,7 +330,7 @@ julia> lambertwbp(1e-18, -1)
 -2.331643983409312e-9
 
 # Same result, but 1000 times slower
-julia> convert(Float64,(lambertw(-BigFloat(1)/e + BigFloat(10)^(-18),-1) + 1))
+julia> convert(Float64, (lambertw(-BigFloat(1)/e + BigFloat(10)^(-18), -1) + 1))
 -2.331643983409312e-9
 ```
 
@@ -349,7 +339,7 @@ julia> convert(Float64,(lambertw(-BigFloat(1)/e + BigFloat(10)^(-18),-1) + 1))
     The loss of precision in `lambertw` is analogous to the loss of precision
     in computing the `sqrt(1-x)` for `x` close to `1`.
 """
-function lambertwbp(x::Number,k::Integer)
+function lambertwbp(x::Number, k::Integer)
     k == 0 && return _lambertw0(x)
     k == -1 && return _lambertwm1(x)
     throw(ArgumentError("expansion about branch point only implemented for k = 0 and -1."))
--- test/lambertw.jl
+++ test/lambertw.jl
@@ -1,94 +1,90 @@
-using Compat
-
-import Compat.MathConstants
-
 ### domain errors
 
-@test_throws DomainError lambertw(-2.0,0)
-@test_throws DomainError lambertw(-2.0,-1)
-@test_throws DomainError lambertw(-2.0,1)
+@test_throws DomainError lambertw(-2.0, 0)
+@test_throws DomainError lambertw(-2.0, -1)
+@test_throws DomainError lambertw(-2.0, 1)
 @test isnan(lambertw(NaN))
 
 ## math constant e
-@test_throws DomainError lambertw(MathConstants.e,1)
-@test_throws DomainError lambertw(MathConstants.e,-1)
+@test_throws DomainError lambertw(MathConstants.e, 1)
+@test_throws DomainError lambertw(MathConstants.e, -1)
 
 ## integer arguments return floating point types
-@test typeof(lambertw(0)) <: AbstractFloat
+@test lambertw(0) isa AbstractFloat
 @test lambertw(0) == 0
 
 ### math constant, MathConstants.e e
 
 # could return math const e, but this would break type stability
-@test typeof(lambertw(1)) <: AbstractFloat
-@test lambertw(MathConstants.e,0) == 1
+@test lambertw(1) isa AbstractFloat
+@test lambertw(MathConstants.e, 0) == 1
 
 ## value at branch point where real branches meet
-@test lambertw(-1/MathConstants.e,0) == lambertw(-1/MathConstants.e,-1) == -1
-@test typeof(lambertw(-1/MathConstants.e,0)) == typeof(lambertw(-1/MathConstants.e,-1)) <: AbstractFloat
+@test lambertw(-inv(MathConstants.e), 0) == lambertw(-inv(MathConstants.e), -1) == -1
+@test typeof(lambertw(-inv(MathConstants.e), 0)) == typeof(lambertw(-inv(MathConstants.e), -1)) <: AbstractFloat
 
 ## convert irrationals to float
 
 @test isapprox(lambertw(pi), 1.0736581947961492)
-@test isapprox(lambertw(pi,0), 1.0736581947961492)
+@test isapprox(lambertw(pi, 0), 1.0736581947961492)
 
 ### infinite args or return values
 
-@test lambertw(0,-1) == lambertw(0.0,-1) == -Inf
-@test lambertw(Inf,0) == Inf
-@test lambertw(complex(Inf,1),0) == complex(Inf,1)
-@test lambertw(complex(Inf,0),1) == complex(Inf,2pi)
-@test lambertw(complex(-Inf,0),1) == complex(Inf,3pi)
-@test lambertw(complex(0.0,0.0),-1) == complex(-Inf,0.0)
+@test lambertw(0, -1) == lambertw(0.0, -1) == -Inf
+@test lambertw(Inf, 0) == Inf
+@test lambertw(complex(Inf, 1), 0) == complex(Inf, 1)
+@test lambertw(complex(Inf, 0), 1) == complex(Inf, 2pi)
+@test lambertw(complex(-Inf, 0), 1) == complex(Inf, 3pi)
+@test lambertw(complex(0.0, 0.0), -1) == complex(-Inf, 0.0)
 
 ## default branch is  k = 0
-@test lambertw(1.0) == lambertw(1.0,0)
+@test lambertw(1.0) == lambertw(1.0, 0)
 
 ## BigInt args return BigFloats
 @test typeof(lambertw(BigInt(0))) == BigFloat
 @test typeof(lambertw(BigInt(3))) == BigFloat
 
 ## Any Integer type allowed for second argument
-@test lambertw(-0.2,-1) == lambertw(-0.2,BigInt(-1))
+@test lambertw(-0.2, -1) == lambertw(-0.2, BigInt(-1))
 
 ## BigInt for second arg does not promote the type
-@test typeof(lambertw(-0.2,-1)) == typeof(lambertw(-0.2,BigInt(-1)))
+@test typeof(lambertw(-0.2, -1)) == typeof(lambertw(-0.2, BigInt(-1)))
 
-for (z,k,res) in [ (0,0 ,0), (complex(0,0),0 ,0),
-                   (complex(0.0,0),0 ,0), (complex(1.0,0),0, 0.567143290409783873) ]
+for (z, k, res) in [(0, 0 , 0), (complex(0, 0), 0 , 0),
+                    (complex(0.0, 0), 0 , 0), (complex(1.0, 0), 0, 0.567143290409783873)]
     if Int != Int32
-        @test isapprox(lambertw(z,k), res)
+        @test isapprox(lambertw(z, k), res)
         @test isapprox(lambertw(z), res)
     else
-        @test isapprox(lambertw(z,k), res; rtol = 1e-14)
+        @test isapprox(lambertw(z, k), res; rtol = 1e-14)
         @test isapprox(lambertw(z), res; rtol = 1e-14)
     end
 end
 
-for (z,k) in ((complex(1,1),2), (complex(1,1),0),(complex(.6,.6),0),
-              (complex(.6,-.6),0))
+for (z, k) in ((complex(1, 1), 2), (complex(1, 1), 0), (complex(.6, .6), 0),
+              (complex(.6, -.6), 0))
     let w
-        @test (w = lambertw(z,k) ; true)
+        @test (w = lambertw(z, k) ; true)
         @test abs(w*exp(w) - z) < 1e-15
     end
 end
 
-@test abs(lambertw(complex(-3.0,-4.0),0) - Complex(1.075073066569255, -1.3251023817343588)) < 1e-14
-@test abs(lambertw(complex(-3.0,-4.0),1) - Complex(0.5887666813694675, 2.7118802109452247)) < 1e-14
-@test (lambertw(complex(.3,.3),0); true)
+@test abs(lambertw(complex(-3.0, -4.0), 0) - Complex(1.075073066569255, -1.3251023817343588)) < 1e-14
+@test abs(lambertw(complex(-3.0, -4.0), 1) - Complex(0.5887666813694675, 2.7118802109452247)) < 1e-14
+@test (lambertw(complex(.3, .3), 0); true)
 
 # bug fix
 # The routine will start at -1/e + eps * im, rather than -1/e + 0im,
 # otherwise root finding will fail
 if Int != Int32
-    @test abs(lambertw(-1.0/MathConstants.e  + 0im,-1)) == 1
+    @test abs(lambertw(-inv(MathConstants.e) + 0im, -1)) == 1
 else
-    @test abs(lambertw(-1.0/MathConstants.e  + 0im,-1) + 1) < 1e-7
+    @test abs(lambertw(-inv(MathConstants.e) + 0im, -1) + 1) < 1e-7
 end
 # lambertw for BigFloat is more precise than Float64. Note
 # that 70 digits in test is about 35 digits in W
 let W
-    for z in [ BigFloat(1),  BigFloat(2), complex(BigFloat(1), BigFloat(1))]
+    for z in [BigFloat(1), BigFloat(2), complex(BigFloat(1), BigFloat(1))]
         @test (W = lambertw(z); true)
         @test abs(z - W * exp(W)) < BigFloat(1)^(-70)
     end
@@ -104,16 +100,16 @@ let sp = precision(BigFloat)
 end
 
 @test lambertw(1) == float(SpecialFunctions.omega)
-@test convert(Float16,SpecialFunctions.omega) == convert(Float16,0.5674)
-@test convert(Float32,SpecialFunctions.omega) == 0.56714326f0
+@test convert(Float16, SpecialFunctions.omega) == convert(Float16, 0.5674)
+@test convert(Float32, SpecialFunctions.omega) == 0.56714326f0
 @test lambertw(BigInt(1)) == big(SpecialFunctions.omega)
 
 ###  expansion about branch point
 
 # not a domain error, but not implemented
-@test_throws ArgumentError lambertwbp(1,1)
+@test_throws ArgumentError lambertwbp(1, 1)
 
-@test_throws DomainError lambertw(.3,2)
+@test_throws DomainError lambertw(.3, 2)
 
 # Expansions about branch point converges almost to machine precision
 # except near the radius of convergence.
@@ -125,7 +121,7 @@ if Int != Int32
 let sp = precision(BigFloat), z = BigFloat(1)/10^12, wo, xdiff
     setprecision(2048)
     for i in 1:300
-        innerarg = z-1/big(MathConstants.e)
+        innerarg = z - inv(big(MathConstants.e))
 
         # branch k = 0
         @test (wo = lambertwbp(Float64(z)); xdiff = abs(-1 + wo - lambertw(innerarg)); true)
@@ -135,7 +131,7 @@ let sp = precision(BigFloat), z = BigFloat(1)/10^12, wo, xdiff
         @test xdiff < 5e-16
 
         #  branch k = -1
-        @test (wo = lambertwbp(Float64(z),-1); xdiff = abs(-1 + wo - lambertw(innerarg,-1)); true)
+        @test (wo = lambertwbp(Float64(z), -1); xdiff = abs(-1 + wo - lambertw(innerarg, -1)); true)
         if xdiff > 5e-16
             println(Float64(z), " ", Float64(xdiff))
         end
@@ -149,8 +145,8 @@ end
 
 # test the expansion about branch point for k=-1,
 # by comparing to exact BigFloat calculation.
-@test lambertwbp(1e-20,-1) - 1 - lambertw(-BigFloat(1)/big(MathConstants.e)+ BigFloat(1)/BigFloat(10)^BigFloat(20),-1) < 1e-16
+@test lambertwbp(1e-20, -1) - 1 - lambertw(-inv(big(MathConstants.e)) + BigFloat(10)^BigFloat(-20), -1) < 1e-16
 
-@test abs(lambertwbp(Complex(.01,.01),-1) - Complex(-0.2755038208041206, -0.1277888928494641)) < 1e-14
+@test abs(lambertwbp(Complex(.01, .01), -1) - Complex(-0.2755038208041206, -0.1277888928494641)) < 1e-14
 
 end  # if Int != Int32
