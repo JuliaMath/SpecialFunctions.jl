e103ccf9c9979c15cd7c098f7f1270608ba311d3
Brian SINQUIN <paradisepx@gmail.com>
2022-07-14 14:05:02 +0200

    Added all function test
    Added the test for the function fresnel(z) for codecov

--- test/fresnel.jl
+++ test/fresnel.jl
@@ -6,4 +6,7 @@
     @test fresnelc(1.)      ≈ 0.7798934003768
     @test fresnels(sqrt(2)) ≈ 0.7139722140219
     @test fresnelc(sqrt(2)) ≈ 0.5288915951112
+    
+    z = rand(ComplexF64)
+    @test fresnel(z) == (fresnelc(z),fresnels(z))
 end
