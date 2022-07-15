97237439343d372b7e88c183f8654fe740344464
Brian SINQUIN <paradisepx@gmail.com>
2022-07-15 20:45:39 +0200

    Update src/fresnel.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/fresnel.jl
+++ src/fresnel.jl
@@ -36,7 +36,18 @@ end
 Calculates the Fresnel sine integral for the number z for
     ``S(z) = \\int_{0}^{z} \\sin{\\left(\\frac{\\pi t^2}{2}\\right)}dt``
 """
-fresnels(z::Number) = 0.25*(1+1im)*(-1im*erf(0.5*(1-1im)*z*√(π)) + erf(0.5*(1+1im)*z*√(π)))
+function fresnels(z::Number)
+    x = (z * sqrtπ) / 2
+    re_x, im_x = reim(x)
+    a = (re_x - im_x) + (re_x - im_x) * im
+    b = (re_x + im_x) + (im_x - re_x) * im
+    re_erf_a, im_erf_a = reim(erf(a))
+    re_erf_b, im_erf_b = reim(erf(b))
+    re_y = (re_erf_a - im_erf_a + re_erf_b + im_erf_b) / 4
+    im_y = (re_erf_a + im_erf_a - re_erf_b + im_erf_b) / 4
+    y = re_y + im_y * im
+    return y
+end
 
 """
         fresnel(z)
