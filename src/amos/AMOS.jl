8f31410a39fd96aef5eff9552e7c5c76688653ac
Chengyu HAN <git@wo-class.cn>
2024-01-21 15:39:00 -0600

    amos: move s1s2 to file

--- src/amos/AMOS.jl
+++ src/amos/AMOS.jl
@@ -9,13 +9,13 @@ _subroutine_names = [
 # No deps, leaf Functions
     "uchk",
     "gammaln",
+    "s1s2",
 # Only deps on leaf functions
 
 # Othsers
 ] # _subroutine_names
 
 for fname in _subroutine_names
-
     include(joinpath("subroutines", "$(fname).jl"))
 end
 
