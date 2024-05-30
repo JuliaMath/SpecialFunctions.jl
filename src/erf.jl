e26f6d391a3026a39ebccd5d7c760af1d0c34688
Abhro R <5664668+abhro@users.noreply.github.com>
2024-05-30 18:01:07 -0400

    Fix spacing

--- src/erf.jl
+++ src/erf.jl
@@ -223,7 +223,7 @@ dawson
 Compute the Faddeeva function of complex `z`, defined by
 ``e^{-z^2} \\operatorname{erfc}(-iz)``.
 Note that this function, also named `w` (original Faddeeva package) or `wofz` (Scilab package),
-is equivalent to``\\operatorname{erfcx}(-iz)``.
+is equivalent to ``\\operatorname{erfcx}(-iz)``.
 """
 faddeeva
 
