cc17155dfe6e127feaf48a0f6395119272dcfa0f
Abhro R <5664668+abhro@users.noreply.github.com>
2024-07-01 14:40:27 -0400

    Fix backtick and backslash use

--- src/erf.jl
+++ src/erf.jl
@@ -618,7 +618,7 @@ end
     logerf(x, y)
 
 Compute the natural logarithm of two-argument error function. This is an accurate version of
- `log(erf(x, y))`, which works for large `x, y`.
+`log(erf(x, y))`, which works for large `x`, `y`.
 
 External links: [Wikipedia](https://en.wikipedia.org/wiki/Error_function).
 
--- src/gamma_inc.jl
+++ src/gamma_inc.jl
@@ -1190,7 +1190,7 @@ Returns the log of the upper incomplete gamma function [`gamma(a,x)`](@ref):
 supporting arbitrary real or complex `a` and `x`.
 
 If `a` and/or `x` is complex, then `exp(loggamma(a,x))` matches `gamma(a,x)` (up to floating-point error),
-but `loggamma(a,x)` may differ from `log(gamma(a,x))` by an integer multiple of ``2\\pi i``
+but `loggamma(a,x)` may differ from `log(gamma(a,x))` by an integer multiple of ``2\pi i``
 (i.e. it may employ a different branch cut).
 
 See also [`loggamma(x)`](@ref).
